698179c0745c7fe65060bac5d8d073da06f7e623
rravenorr <148618881+rravenorr@users.noreply.github.com>
2024-08-27 11:59:52 +0800

    1

--- Laravel_app/lara_app/resources/views/welcome.blade.php
+++ Laravel_app/lara_app/resources/views/welcome.blade.php
@@ -8,7 +8,11 @@
         <title>Tanginamo</title>
 
     </head>
-    <body class="">
+    <body class="bg-green-500 flex justify-center items-center h-screen w-screen">
+        {{-- <div class="text-center animate-spin ">
+        <span class="text-5xl font-extrabold text-red-700 rotate-180">WEB DESIGN IS MY PASSION</span>
         
+    </div> --}}
+    <div class="h-24 w-24 bg-red-500 animate-spin duration-1000"></div>
     </body>
 </html>
