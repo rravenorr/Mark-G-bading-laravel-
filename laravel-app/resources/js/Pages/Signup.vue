88d048f748a7c310c80d30bc2d8f007fc05066f5
rravenorr <148618881+rravenorr@users.noreply.github.com>
2024-09-03 07:57:11 +0800

    1

--- laravel-app/resources/js/Pages/Signup.vue
+++ laravel-app/resources/js/Pages/Signup.vue
@@ -11,12 +11,12 @@
         <h1 class="text-2xl font-semibold mb-6">SIGN UP</h1>
         <form method="POST" action="{{ route('login') }}"  id="loginForm">
             <div class="mb-4">
-                <label for="username" class="block text-sm font-medium text-gray-700">First name</label>
-                <input type="text" name="username" id="username" autocomplete="username" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
+                <label for="firstname" class="block text-sm font-medium text-gray-700">First name</label>
+                <input type="text" name="firstname" id="firstname" autocomplete="username" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
             </div>
             <div class="mb-6">
-                <label for="password" class="block text-sm font-medium text-gray-700">Last name</label>
-                <input type="password" name="password" id="password" autocomplete="current-password" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
+                <label for="lastname" class="block text-sm font-medium text-gray-700">Last name</label>
+                <input type="text" name="lastname" id="lastname" autocomplete="current-password" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
             </div>
             <div class="mb-4">
                 <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
@@ -27,8 +27,8 @@
                 <input type="password" name="password" id="password" autocomplete="current-password" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
             </div>
             <div class="mb-6">
-                <label for="password" class="block text-sm font-medium text-gray-700">Re-type Password</label>
-                <input type="password" name="password" id="password" autocomplete="current-password" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
+                <label for="passwordr" class="block text-sm font-medium text-gray-700">Re-type Password</label>
+                <input type="password" name="passwordr" id="passwordr" autocomplete="current-password" class="min-h-[40px] pl-4 mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
             </div>
             <div class="flex justify-end mb-6">
                 <button type="submit" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
